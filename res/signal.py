812acbe980f978e6863c36e3716c991533048a68
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2023-08-25 00:22:56 +0900

    書き出し準備

--- res/signal.py
+++ res/signal.py
@@ -3,7 +3,10 @@ import tomllib
 
 os.chdir(os.path.dirname(__file__))
 
-with open("signal.toml", "rb") as f:
-    data = tomllib.load(f)
+def lever_lua_code(name, data):
+    return ""
+
+with (open("signal.toml", "rb") as toml_f, open("signal.lua", "w") as lua_f):
+    data = tomllib.load(toml_f)
     for k, v in data.items():
-        print(k, v)
+        print(lever_lua_code(k, v), file = lua_f)
