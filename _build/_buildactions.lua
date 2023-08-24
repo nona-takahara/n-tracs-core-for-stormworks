10f7810245f8db82d84eca0b4e2eb97150211a0c
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2023-08-24 14:05:04 +0900

    データファイル・メイクを設定

---
+++ _build/_buildactions.lua
@@ -0,0 +1,8 @@
+-- Author: <Authorname> (Please change this in user settings, Ctrl+Comma)
+-- GitHub: <GithubLink>
+-- Workshop: <WorkshopLink>
+--
+--- Developed using LifeBoatAPI - Stormworks Lua plugin for VSCode - https://code.visualstudio.com/download (search "Stormworks Lua with LifeboatAPI" extension)
+--- If you have any issues, please report them here: https://github.com/nameouschangey/STORMWORKS_VSCodeExtension/issues - by Nameous Changey
+
+os.execute("make_data.bat")
---
+++ res/area_track.lua
@@ -0,0 +1,6 @@
+-- Author: Nona Takahara
+-- GitHub: https://github.com/nona-takahara/n-tracs-soya-express
+-- Workshop: <WorkshopLink>
+--
+--- Developed using LifeBoatAPI - Stormworks Lua plugin for VSCode - https://code.visualstudio.com/download (search "Stormworks Lua with LifeboatAPI" extension)
+--- If you have any issues, please report them here: https://github.com/nameouschangey/STORMWORKS_VSCodeExtension/issues - by Nameous Changey
