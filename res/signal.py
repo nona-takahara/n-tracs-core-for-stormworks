985b33b9ff16d5b02e793de910b3ef83954a9552
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2023-08-26 17:29:48 +0900

    信号データ生成の仮バージョンを完成

--- res/signal.py
+++ res/signal.py
@@ -4,7 +4,42 @@ import tomllib
 os.chdir(os.path.dirname(__file__))
 
 def lever_lua_code(name, data):
-    return ""
+    switchesMake=[]
+    for v in data["switches"]:
+        switchesMake.append(f'SwitchRoute.new(SwitchGetter"${v["sw"]}",TargetRoute.{v["t"].capitalize()})')
+    
+    routeLockMake=[]
+    for v in data["route_lock"]:
+        routeLockMake.append(f'TrackGetter"{v}"')
+
+    overrunLockMake=[]
+    for v in data["overrun_lock"]:
+        overrunLockMake.append(f'TrackGetter"{v}"')
+
+    signalTrackMake=[]
+    for v in data["signal_track"]:
+        signalTrackMake.append(f'TrackGetter"{v}"')
+
+    approachTrackMake=[]
+    for v in data["approach_track"]:
+        approachTrackMake.append(f'TrackGetter"{v}"')
+
+    rets = 'Lever.renew(' + \
+        f'LeverGetter"${name}",' +\
+        f'"${name}",' +\
+        f'TrackGetter"{data["start"]}",' + \
+        f'TrackGetter"{data["destination"]}",' + \
+        '{' + f'{",".join(switchesMake)}' + '},' + \
+        '{' + ','.join(routeLockMake) + '},' +\
+        '{' + ','.join(overrunLockMake) + '},' +\
+        '{' + ','.join(signalTrackMake) + '},' +\
+        f'RouteDirection.{data["direction"].capitalize()},' +\
+        f'{data["approach_lock_time"]},' +\
+        f'{data["overrun_lock_time"]},' +\
+        f'{data["update_callback"]}' +\
+    ')'
+
+    return rets
 
 with (open("signal.toml", "rb") as toml_f, open("signal.lua", "w") as lua_f):
     data = tomllib.load(toml_f)
