002e785a4a3118ab3b8cf49c348f8ada2c42ecf7
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2023-09-19 17:16:01 +0900

    コマンドシステムを導入

---
+++ src/n_tracs_soyabridge/command.lua
@@ -0,0 +1,41 @@
+---@class Command
+---@field admin boolean
+---@field auth boolean
+---@field description string
+---@field command function
+
+---@type table<string,Command>
+COMMANDS = {}
+
+COMMANDS["help"] = {
+    admin = false,
+    auth = false,
+    description = "Get help for usage of N-TRACS Soya Express Wayside Signals",
+    command = (function(_, is_admin, is_auth, peer_id)
+        for key, cmd in pairs(COMMANDS) do
+            if (not cmd.admin or (cmd.admin and is_admin)) and (not cmd.auth or (cmd.auth and is_auth)) then
+                Announce("?nt " .. key .. ": " .. cmd.description, peer_id)
+            end
+        end
+    end)
+}
+
+function Announce(message, peer_id)
+    server.announce("[Soya Express WS]", message, peer_id)
+end
+
+function onCustomCommand(full_message, peer_id, is_admin, is_auth, command, ...)
+    if command == "?ntracs" or command == "?nt" then
+        local args = { ... }
+        if COMMANDS[args[1]] then
+            local cmd = COMMANDS[args[1]]
+            if (not cmd.admin or (cmd.admin and is_admin)) and (not cmd.auth or (cmd.auth and is_auth)) then
+                cmd.command(args, is_admin, is_auth, peer_id)
+            end
+        end
+    end
+
+    if command == "?help" then
+        Announce("For more help, use ?nt help", peer_id)
+    end
+end
