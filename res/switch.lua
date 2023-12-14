ec9be57f7c840611a2dc5a07da3ee4807d450cda
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2023-12-14 21:46:16 +0900

    てっ査鎖錠のミスを修正

--- res/switch.lua
+++ res/switch.lua
@@ -5,8 +5,8 @@ CreateSwitch("NHB31", { "NHB31" }, { TrackGetter("NHB22T") }, true)
 CreateSwitch("WAK11", { "WAK11" }, { TrackGetter("WAK1RBT"), TrackGetter("WAK11T") })
 CreateSwitch("WAK12", { "WAK12" }, { TrackGetter("WAK12T") })
 
-CreateSwitch("SGN11", { "SGN11" }, { TrackGetter("SGN1RBT"), TrackGetter("SGN11T") })
-CreateSwitch("SGN12", { "SGN12" }, { TrackGetter("SGN12T") })
+CreateSwitch("SGN21", { "SGN21" }, { TrackGetter("SGN1RT"), TrackGetter("SGN21T") })
+CreateSwitch("SGN22", { "SGN22" }, { TrackGetter("SGN22T") })
 
 CreateSwitch("SNH21", { "SNH21a", "SNH21b" }, { TrackGetter("SNH21AT"), TrackGetter("SNH21BT") })
 CreateSwitch("SNH22", { "SNH22" }, { TrackGetter("SNH22T") })
