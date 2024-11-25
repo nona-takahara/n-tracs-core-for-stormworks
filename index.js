dd68f564ea4c4c2fa4b29cf3ca8ae94be4ea3c0e
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2024-11-25 10:45:05 +0900

    仮のビルドスクリプトを完成

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-const fs = require("node:fs");
-const luaparse = require("luaparse");
-const luamin = require("./src-js/luamin");
+const { execSync } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+require('dotenv').config();
 
-const f = fs.readFileSync("script.lua").toString();
+process.chdir(__dirname);
 
+if (fs.statSync("res/area_track.json").mtime > fs.statSync("res/area_track.lua").mtime) {
+    console.log(execSync("python res/area_track.py").toString());
+}
 
-const ast = luaparse.parse(f, {
-    comments: true,
-    scope: true,
-    locations: true,
-    luaVersion: '5.3'
-})
-console.log(JSON.stringify({"ast": ast, "code": luamin.minify(ast)}));
+if (fs.statSync("res/signal.toml").mtime > fs.statSync("res/signal.lua").mtime) {
+    console.log(execSync("python res/signal.py").toString());
+}
+
+console.log(execSync("npx storm-lua-minify script.lua").toString());
+if (!fs.existsSync("dist")) {
+    fs.mkdirSync("dist");
+}
+
+fs.copyFileSync("script.lua.map", "dist/script.lua.map");
+fs.copyFileSync("script.min.lua", "dist/script.min.lua");
+
+if(process.env.NTRACS_SW_DIR) {
+    fs.copyFileSync("script.min.lua", path.join(process.env.NTRACS_SW_DIR, "script.lua"));
+}
+
+fs.rmSync("script.lua.map");
+fs.rmSync("script.min.lua");
+
+console.log("Copied! "+ (new Date().toLocaleTimeString()));
