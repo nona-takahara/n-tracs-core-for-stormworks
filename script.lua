9c254d294f82fa85d64adf67cdef416d075dad32
Nona Takahara <nona_t_2612@yahoo.co.jp>
2026-07-01 23:39:36 +0900

    掘戸線開業及び単線自動閉そく改修完了

--- script.lua
+++ script.lua
@@ -1,6 +1,6 @@
 ADDON_NAME = "N-TRACS Soya Express Wayside Signals"
 ADDON_SHORT_NAME = "SoyaExpress WS"
-ADDON_VERSION = "v2.0.0-beta4"
+ADDON_VERSION = "v2.0.0-beta5"
 CTC_VERSION = "SoyaWS-3"
 
 error = error or function(message)
